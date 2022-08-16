9831b399326a4b3f63f76aa13307b009c0890fb4
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2022-08-16 18:29:56 -0400

    add dotenv dependency, .env.example. and associated code in ConnectionUtility

--- src/main/java/com/revature/model/User.java
+++ src/main/java/com/revature/model/User.java
@@ -15,7 +15,7 @@ public class User {
     private String userRole;
 
 
-   // private List<Account> accounts;
+    private List<Account> accounts;
 
 
 
@@ -138,7 +138,6 @@ public class User {
               //  ", accounts=" + accounts +
                 '}';
     }
-    
-                '}';
+
 
 }
