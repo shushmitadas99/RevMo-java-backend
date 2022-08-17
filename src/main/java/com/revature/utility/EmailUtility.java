36646eca00011d45208051e45dca16c2653376d4
Mahwishubham <mazizi4646@email.phoenix.edu>
2022-08-17 13:42:46 -0400

    removed secrets from code

--- src/main/java/com/revature/utility/EmailUtility.java
+++ src/main/java/com/revature/utility/EmailUtility.java
@@ -5,19 +5,22 @@ import java.sql.Connection;
 
 public class EmailUtility {
 
+    // Test
     public static void main(String[] args) throws Exception {
-        Connection conn = ConnectionUtility.createConnection();
-       // email("mazizi.c@gmail.com", "Reset Password Link", "We will be sending an html forget password template");
+       // Connection conn = ConnectionUtility.createConnection();
+       email("mazizi.c@gmail.com",
+               "Reset your RevMo password",
+               "<strong>We will be sending an html forget password template</strong>");
     }
     public static void email(String email_to, String sub, String email_content) throws IOException {
 
         Email from = new Email("revmobank@gmail.com");
         String subject = sub;
         Email to = new Email(email_to);
-        Content content = new Content("text/plain", email_content);
+        Content content = new Content("text/html", email_content);
 
         Mail mail = new Mail(from, subject, to, content);;
-        SendGrid sg = new SendGrid("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");
+        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
 
         Request request = new Request();
         try {
