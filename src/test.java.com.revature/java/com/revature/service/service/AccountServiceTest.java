a0d563f939c305a2d0580dfbe6bbcaffb42dcefc
Chris Sullivan <christopsullivan@outlook.com>
2022-08-18 14:06:49 -0700

    more unit test added to accounts test

--- src/test.java.com.revature/java/com/revature/service/service/AccountServiceTest.java
+++ src/test.java.com.revature/java/com/revature/service/service/AccountServiceTest.java
@@ -1,13 +1,17 @@
 package com.revature.service.service;
 
 import com.revature.dao.AccountDao;
+import com.revature.exception.InvalidParameterException;
 import com.revature.model.Account;
 import com.revature.service.AccountService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -36,5 +40,56 @@ public class AccountServiceTest {
         List<Account> expected = new ArrayList<>(accountList);
         Assertions.assertEquals(expected, actual);
     }
+    @Test
+    public void testOpenAccountPositive() throws InvalidParameterException {
+        // Arrange
 
-}
+        AccountDao mockedObject = mock(AccountDao.class);
+        Account mockAccount = new Account(0, 1, 0);
+        Account returnAccount = new Account(1,1,null, 0);
+        Map<String, String> addAccount = new HashMap<>();
+        addAccount.put("typeId", "1");
+        addAccount.put("balance", "0");
+
+        // Whenever the code in the Service layer calls the getAllStudents() method
+        // for the dao layer, then return the list of students
+        // we have specified above
+        when(mockedObject.openAccount(mockAccount)).thenReturn(returnAccount);
+
+        AccountService accountService = new AccountService(mockedObject);
+
+        // Act
+        Account actual = accountService.openAccount(addAccount);
+//
+//        // Assert
+        Account expected = new Account(1,1,0);
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testGetAccountsByEmailandAccountId() throws SQLException {
+        // Arrange
+        AccountDao mockedObject = mock(AccountDao.class);
+        String email = "jd80@a.ca";
+        int id = 3;
+
+        Account account = (new Account(1, 1, 20));
+        Account account2 =(new Account(2, 1, 15));
+        Account account3 =(new Account(3, 1, 30));
+
+        // Whenever the code in the Service layer calls the getAllStudents() method
+        // for the dao layer, then return the list of students
+        // we have specified above
+        when(mockedObject.getAccountByEmailAndAccountId(email, id)).thenReturn(account3);
+
+        AccountService accountService = new AccountService(mockedObject);
+
+        // Act
+        Account actual = accountService.getAccountByEmailAndAccountId(email, id);
+//
+//        // Assert
+        Account expected = new Account(3,1,30);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    }
+
