560c97ef29adaa39dabbc4e711c1541c57bca1f9
Chris Sullivan <christopsullivan@outlook.com>
2022-08-18 12:47:26 -0700

    first unit test done

--- src/test.java.com.revature/java/com/revature/service/service/AccountServiceTest.java
+++ src/test.java.com.revature/java/com/revature/service/service/AccountServiceTest.java
@@ -1,4 +1,40 @@
 package com.revature.service.service;
 
+import com.revature.dao.AccountDao;
+import com.revature.model.Account;
+import com.revature.service.AccountService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class AccountServiceTest {
+    @Test
+    public void testGetAccountsByEmail() throws SQLException {
+        // Arrange
+        AccountDao mockedObject = mock(AccountDao.class);
+        String email = "jd80@a.ca";
+        List<Account> accountList = new ArrayList<>();
+        accountList.add(new Account(1, 1, 20));
+        accountList.add(new Account(2, 1, 15));
+        accountList.add(new Account(3, 1, 30));
+
+        // Whenever the code in the Service layer calls the getAllStudents() method
+        // for the dao layer, then return the list of students
+        // we have specified above
+        when(mockedObject.getAccountsByEmail(email)).thenReturn(accountList);
+
+        AccountService accountService = new AccountService(mockedObject);
+
+        // Act
+        List<Account> actual = accountService.getAccountsByEmail(email);
+//
+//        // Assert
+        List<Account> expected = new ArrayList<>(accountList);
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
