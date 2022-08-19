2c53b57e571f92aa8beb9c6dfbc3bcd221ed36e5
scott <scottsvance7@gmail.com>
2022-08-19 17:32:08 -0400

    play with transactions so valentin has something to edit

--- src/main/java/com/revature/dao/TransactionDao.java
+++ src/main/java/com/revature/dao/TransactionDao.java
@@ -112,7 +112,7 @@ public class TransactionDao {
                 String typeName = rs.getString("type_name");
                 String description = rs.getString("description");
                 Transaction transaction = new Transaction(transactionId, requesterId,
-                        sendingId, receivingId, reqTime, resTime,receivingEmail,
+                        sendingId, receivingId, reqTime, resTime, receivingEmail,
                         initiatedBy, typeName, description, amount);
                 transactionsList.add(transaction);
             }
@@ -144,13 +144,14 @@ public class TransactionDao {
                 String typeName = rs.getString("type_name");
                 String description = rs.getString("description");
                 Transaction transaction = new Transaction(transactionId, requesterId,
-                        sendingId, receivingId, reqTime, resTime,receivingEmail,
+                        sendingId, receivingId, reqTime, resTime, receivingEmail,
                         initiatedBy, typeName, description, amount);
                 transactionsList.add(transaction);
             }
             return transactionsList;
         }
     }
+
     public List<Transaction> getAllTransactionsByRecievingId(int receiveId) throws SQLException {
         try (Connection con = ConnectionUtility.createConnection()) {
             PreparedStatement ps = con.prepareStatement("SELECT t.id, t.requester_id, t.sending_id, t.receiving_id, t.req_time, t.res_time, t.amount, t.receiving_email,  concat_ws(' ', u.first_name,u.last_name) as initiated_by, st.type_name, td.description" +
@@ -175,13 +176,14 @@ public class TransactionDao {
                 String typeName = rs.getString("type_name");
                 String description = rs.getString("description");
                 Transaction transaction = new Transaction(transactionId, requesterId,
-                        sendingId, receivingId, reqTime, resTime,receivingEmail,
+                        sendingId, receivingId, reqTime, resTime, receivingEmail,
                         initiatedBy, typeName, description, amount);
                 transactionsList.add(transaction);
             }
             return transactionsList;
         }
     }
+
     public List<Transaction> getAllTransactionsByStatusName(String statusName) throws SQLException {
         try (Connection con = ConnectionUtility.createConnection()) {
             PreparedStatement ps = con.prepareStatement("SELECT t.id, t.requester_id, t.sending_id, t.receiving_id, t.req_time, t.res_time, t.amount, t.receiving_email,  concat_ws(' ', u.first_name,u.last_name) as initiated_by, st.type_name, td.description" +
@@ -206,7 +208,7 @@ public class TransactionDao {
                 String typeName = rs.getString("type_name");
                 String description = rs.getString("description");
                 Transaction transaction = new Transaction(transactionId, requesterId,
-                        sendingId, receivingId, reqTime, resTime,receivingEmail,
+                        sendingId, receivingId, reqTime, resTime, receivingEmail,
                         initiatedBy, typeName, description, amount);
                 transactionsList.add(transaction);
             }
@@ -238,21 +240,77 @@ public class TransactionDao {
                 String typeName = rs.getString("type_name");
                 String description = rs.getString("description");
                 Transaction transaction = new Transaction(transactionId, requesterId,
-                        sendingId, receivingId, reqTime, resTime,receivingEmail,
+                        sendingId, receivingId, reqTime, resTime, receivingEmail,
                         initiatedBy, typeName, description, amount);
                 transactionsList.add(transaction);
             }
             return transactionsList;
         }
     }
+
+//    public Transaction sendMoneyRequest(Transaction transaction) throws SQLException {
+//        try (Connection con = ConnectionUtility.createConnection()) {
+//            con.setAutoCommit(false);
+//            try (
+//                    PreparedStatement ps = con.prepareStatement("INSERT INTO transactions (requester_id, " +
+//                            "sending_id,receiving_id, req_time, status_id, amount, desc_id, receiving_email) " +
+//                            "VALUES(?, ?, ?, Now(),?, ?, ?,?) RETURNING *");
+//            ) {
+//                ps.setInt(1, transaction.getRequesterId());
+//                ps.setInt(2, transaction.getSendingId());
+//                ps.setInt(3, transaction.getReceivingId());
+//                ps.setInt(4, transaction.getStatusId());
+//                ps.setLong(5, transaction.getAmount());
+//                ps.setInt(6, transaction.getDescriptionId());
+//                ps.setString(7, transaction.getReceivingEmail());
+//                ps.executeUpdate();
+//                ResultSet rs = ps.executeQuery();
+//                while (rs.next()) {
+//                    int transactionId = rs.getInt("id");
+//                    int requesterId = rs.getInt("requester_id");
+//                    int sendingId = rs.getInt("sending_id");
+//                    int receivingId = rs.getInt("receiving_id");
+//                    Timestamp reqTime = rs.getTimestamp("req_time");
+//                    long amount = rs.getLong("amount");
+//                    String receivingEmail = rs.getString("receiving_email");
+//                    String initiatedBy = rs.getString("initiated_by");
+//                    String typeName = rs.getString("type_name");
+//                    String description = rs.getString("description");
+//                    Transaction trx = new Transaction(transactionId, requesterId,
+//                            sendingId, receivingId, reqTime, receivingEmail,
+//                            initiatedBy, typeName, description, amount);
+//                    return trx;
+//                }
+//
+//            } catch (SQLException e) {
+//                System.out.println(e);
+//            }
+//            return null;
+//        }
+//}
+            /*
+            This method will initiate a money transfer request. The request will then need to be approved by the person
+            who would be transferring money to the requester.
+             */
+
+    public String completeMoneyRequest() throws SQLException {
+        /*
+        This method will finish the money transfer request. The sender will approve or deny the money transfer request.
+         */
+        return null;
+    }
+
+    public String cancelTransaction() throws SQLException {
+        /*
+        This method will cancel a transaction if it has not been approved OR denied.
+         */
+        return null;
+    }
 }
 
 
 
-//
-//
-//    //              (update(approve/deny) give resolve time, descriptionId, change amount in accounts
-////delete-cancel a transaction as long as it hasnt been approved
-//
-//}
-//
+
+
+// (update(approve/deny) give resolve time, descriptionId, change amount in accounts
+// delete-cancel a transaction as long as it hasnt been approved
