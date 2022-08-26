b1f3d764268fde79f2758bf970dd259f41381405
Aidan Carter <aidan.kyle.carter@gmail.com>
2022-08-26 14:52:23 -0700

    finish edit user info

--- src/main/java/com/revature/dao/UserDao.java
+++ src/main/java/com/revature/dao/UserDao.java
@@ -34,4 +34,120 @@ public class UserDao {
             }
         }
     }
+<<<<<<< Updated upstream
+=======
+
+    public User getUserByEmail(String email) {
+        try (Connection con = ConnectionUtility.createConnection()) {
+            PreparedStatement pstmt = con.prepareStatement("SELECT * FROM users WHERE email=?");
+
+            pstmt.setString(1, email);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                return new User(rs.getInt("id"), rs.getString("first_name"),
+                        rs.getString("last_name"), rs.getString("email"),
+                        rs.getString("pass"), rs.getString("phone"),
+                        rs.getString("role_id"));
+            }
+
+            return null;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public String updateEmail(int userId, String email) {
+        try (Connection con = ConnectionUtility.createConnection()) {
+            PreparedStatement pstmt = con.prepareStatement("UPDATE users SET email=? WHERE id=? RETURNING *");
+            pstmt.setString(1, email);
+            pstmt.setInt(2, userId);
+            ResultSet rs = pstmt.executeQuery();
+
+            return "Email updated.";
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String updatephone(int userId, String phoneNumber) {
+        try (Connection con = ConnectionUtility.createConnection()) {
+            PreparedStatement pstmt = con.prepareStatement("UPDATE users SET phone=? WHERE id=? RETURNING *");
+            pstmt.setString(1, phoneNumber);
+            pstmt.setInt(2, userId);
+            ResultSet rs = pstmt.executeQuery();
+
+            return "phone updated. Congrats?";
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public String updateFirstName(int userId, String firstName) {
+        try (Connection con = ConnectionUtility.createConnection()) {
+            PreparedStatement pstmt = con.prepareStatement("UPDATE users SET first_name=? WHERE id=? RETURNING *");
+            pstmt.setString(1, firstName);
+            pstmt.setInt(2, userId);
+            ResultSet rs = pstmt.executeQuery();
+
+            return "First name updated. Congrats!";
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public String updateLastName(int userId, String lastName) {
+        try (Connection con = ConnectionUtility.createConnection()) {
+            PreparedStatement pstmt = con.prepareStatement("UPDATE users SET last_name=? WHERE id=? RETURNING *");
+            pstmt.setString(1, lastName);
+            pstmt.setInt(2, userId);
+            ResultSet rs = pstmt.executeQuery();
+
+            return "Last name updated. Congrats?";
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getRequesteeEmailByTransactionId(int transactionId) {
+        try (Connection con = ConnectionUtility.createConnection()) {
+            PreparedStatement ps = con.prepareStatement("SELECT u.email FROM users u " +
+                    "JOIN users_with_accounts uwa ON uwa.user_id = u.id " +
+                    "JOIN transactions t ON uwa.account_id = t.sending_id " +
+                    "WHERE t.id = ?");
+            ps.setInt(1, transactionId);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getString("email");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    public List<String> getReceiverEmailByTransactionId(int transactionId) {
+        try (Connection con = ConnectionUtility.createConnection()) {
+            PreparedStatement ps = con.prepareStatement("SELECT u.email FROM users u " +
+                    "JOIN users_with_accounts uwa ON uwa.user_id = u.id " +
+                    "JOIN transactions t ON uwa.account_id = t.receiving_id " +
+                    "WHERE t.id = ?");
+            ps.setInt(1, transactionId);
+            ResultSet rs = ps.executeQuery();
+            List<String> emails = new ArrayList<>();
+            while (rs.next()) {
+                emails.add(rs.getString("email"));
+            }
+            return emails;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+>>>>>>> Stashed changes
 }
--- src/main/java/com/revature/service/UserService.java
+++ src/main/java/com/revature/service/UserService.java
@@ -22,5 +22,79 @@ public class UserService {
         }
         return user;
     }
+<<<<<<< Updated upstream
+=======
+
+    public User getUserByEmail(String email) {
+
+
+        return userDao.getUserByEmail(email);
+
+    }
+
+    public void updateInfo(Map<String, String> newInfo, int userId, String oldEmail) throws InvalidParameterException {
+        InvalidParameterException exceptions = new InvalidParameterException();
+        String newFirstName = newInfo.get("firstName");
+        String newLastName = newInfo.get("lastName");
+        String newPhone = newInfo.get("phone");
+        User oldUser = userDao.getUserByEmail(oldEmail);
+        if (userId != oldUser.getUserId()) {
+            exceptions.addMessage("User Id does not match our records.");
+            throw exceptions;
+        }
+
+        if (!Objects.equals(newFirstName, oldUser.getFirstName())) {
+            userDao.updateFirstName(userId, newFirstName);
+        }
+        if (!Objects.equals(newLastName, oldUser.getLastName())) {
+            userDao.updateLastName(userId, newLastName);
+        }
+        if (!Objects.equals(newPhone, oldUser.getPhoneNumber())) {
+            userDao.updatephone(userId, newPhone);
+        }
+
+    }
+
+
+    public String getRequesteeByTransactionId(int transactionId) {
+        return userDao.getRequesteeEmailByTransactionId(transactionId);
+    }
+
+    public List<String> getReceiverByTransactionId(int transactionId) {
+        return userDao.getReceiverEmailByTransactionId(transactionId);
+    }
+
+    public boolean forgetPassword(JSONObject inputEmail) {
+        try {
+
+            //Check if email is in the database
+            if (userDao.getUserEmailByEmail(inputEmail.getString("email"))) {
+
+                //return user Object based on email found
+                User currUser = userDao.getUserByInputEmail(inputEmail.getString("email"));
+
+                //Create web Token based on values with expiration
+                String jwtToken = Jwts.builder().claim("last_name", currUser.getLastName()).claim("userId", currUser.getUserId()).claim("email", currUser.getEmail()).setSubject(currUser.getFirstName()).setId(UUID.randomUUID().toString()).setIssuedAt(Date.from(Instant.now())).setExpiration(Date.from(Instant.now().plus(5L, ChronoUnit.MINUTES))).compact();
+
+                //Send Token to Database
+                userDao.sendToken(jwtToken, currUser.getUserId());
+                Dotenv dotenv = Dotenv.load();
+                //Create URL and send email with reset URL
+                String frontendUrl = dotenv.get("FRONTEND_HOST");
+                String addressUrl =  frontendUrl +"/resetpassword?token="+jwtToken;
+                int status = EmailUtility.email(inputEmail.getString("email"), "Reset your RevMo password", addressUrl);
+                if (status == 202) {
+                    return true;
+                } else {
+                    throw new RuntimeException("The email pertaining to the account has been sent an email. Please check email for reset link.");
+                }
+            } else {
+                throw new RuntimeException("The email pertaining to the account has been sent an email. Please check email for reset link.");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("The email pertaining to the account has been sent an email. Please check email for reset link.");
+        }
+    }
+>>>>>>> Stashed changes
 }
 
